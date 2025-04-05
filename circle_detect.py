90ec558901b0f3f75bbbaa928bcf785dab9a4f5b
alulacoder <rasalula2024@gmail.com>
2025-04-05 09:58:57 +0100

    Add files via upload

---
+++ circle_detect.py
@@ -0,0 +1,22 @@
+# #### Detection of Circles in a Image
+
+import cv2
+import numpy as np
+
+img = cv2.imread('open_cv/image/pikachu.jpeg', cv2.IMREAD_COLOR)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray_blurred = cv2.blur (gray, (3,3))
+
+# Give a brief overview of what does HoughCirices function do and What are the parameters required.
+detected_circles = cv2. HoughCircles (gray_blurred,cv2.HOUGH_GRADIENT, 1, 20, param1 = 20, param2 = 10, minRadius = 1, maxRadius = 50)
+
+if detected_circles is not None:
+    detected_circles = np.uint16(np.around (detected_circles))
+    for pt in detected_circles [0, :] :
+        a, b, r = pt[0], pt[1], pt[2]
+        cv2.circle(img, (a,b), r, (0, 255, 0), 2)
+        cv2.circle(img, (a, b), 1,( 0, 0, 255), 3)
+        cv2.imshow("Detected Circles", img) 
+        cv2.waitKey(0)
+
+cv2.destroyAllWindows ()
